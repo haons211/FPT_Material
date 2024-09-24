5a4f0ab8a5c9c15ef7173c16b130abd8328aa776
haons211 <syhaoc2dh@gmail.com>
2024-09-24 15:49:59 +0700

    first commit

---
+++ 2024Fall/PRN212/Practice/DemoWindowPage/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemoWindowPage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        // Show page 2
+        private void btnToPage02_Click(object sender, RoutedEventArgs e)
+        {
+            frMain.Content = new Page_02();
+        }
+
+        // Show page 1
+        private void btnToPage01_Click(object sender, RoutedEventArgs e)
+        {
+            frMain.Content = new Page_01();
+        }
+    }
+}
---
+++ 2024Fall/PRN212/Practice/MyWPFApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MyWPFApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
